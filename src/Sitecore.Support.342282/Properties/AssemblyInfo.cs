b4a7138e9c28bb447951a8d11dc5d18b4bf5accf
OlegBraginskyy <obr@sitecore.net>
2019-07-08 17:33:11 +0300

    Init

---
+++ src/Sitecore.Support.342282/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.342282")]
+[assembly: AssemblyProduct("Sitecore.Support.342282")]
+[assembly: ComVisible(false)]
